a85d61bef73e3b3aa3c499b2b41633efc4ce7502
emmanuelkamala <emmanuelkamala86@gmail.com>
2019-09-20 13:37:22 +0300

    Create a User model and add records to the user table

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+    validates :username, presence: true, length: { minimum: 6, maximum: 25 }, uniqueness: true
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 50}, 
+              format: {with: VALID_EMAIL_REGEX}, 
+              uniqueness: {case_sensitive: false}
+end
