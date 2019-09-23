0cbded410cb0dc08f2c4c05ab9dba15789286e7c
Zibilyonik <antagonistpotato@gmail.com>
2019-09-23 19:29:18 +0300

    Linter errors fixed (1/?)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
+# frozen_string_literal: true
 class User < ApplicationRecord
-    validates :username, presence: true, length: { minimum: 6, maximum: 25 }, uniqueness: true
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, length: { maximum: 50}, 
-              format: {with: VALID_EMAIL_REGEX}, 
-              uniqueness: {case_sensitive: false}
+  validates :username, presence: true, length: { minimum: 6, maximum: 25 }, uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 50 },
+            format: { with: VALID_EMAIL_REGEX },
+            uniqueness: { case_sensitive: false }
 
-    has_many :posts
-    has_many :comments
+  has_many :posts
+  has_many :comments
 end
