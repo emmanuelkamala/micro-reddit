3a9c780ad23b9ec1761e0ff7880d7b1f71b9d9a5
emmanuelkamala <emmanuelkamala86@gmail.com>
2019-09-24 10:09:48 +0300

    Add missing space in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@
 # anti-linter comment
 class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 100 }
-  validates :body, presence: true, length: {minimum: 80 }
+  validates :body, presence: true, length: { minimum: 80 }
   belongs_to :user
   has_many :comments
 end
